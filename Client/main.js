6edf519731e599cae460a4b7ecef29b8bcdc9428
Sneaky <SneakyLeif1020@gmail.com>
2019-10-20 22:40:03 -0400

    Added jquery-ui, and redesigned the main menu a bit with added animations.

--- Client/main.js
+++ Client/main.js
@@ -1,9 +1,52 @@
-$(function() {
-	console.log("hi");
-});
+// Initial variables
+var connected = false;
 
-socket = io.connect("http://sneakyleif.com:8081", {
+// socket.io connection
+var socket = io.connect("http://sneakyleif.com:8081", {
 	'reconnection': true,
 	'reconnectionDelay': 500,
 	'reconnectionAttempts': 'Infinity'
+});
+
+// On initial connection
+socket.on('initial-connection', function() {
+	connected = true;
+});
+
+// On page load
+$(function() {
+	var loadingCheck = setInterval(function() {
+		if (connected) {
+			$("#main-menu-container").show();
+			$("#title").show("blind", 250);
+			setTimeout(function() {
+				$("#player-type-container").show("blind", 500);
+			}, 180);
+			clearInterval(loadingCheck);
+		}
+	}, 100);
+});
+
+$(document).on("click", "#new", () => {
+	$("#player-type-container").hide("blind", {
+		"direction": "down"
+	}, 250);
+	$("#register-container").show("blind", 500);
+});
+
+$(document).on("click", "#old", () => {
+	$("#player-type-container").hide("blind", {
+		"direction": "down"
+	}, 250);
+	$("#login-container").show("blind", 500);
+});
+
+$(document).on("click", "#back", () => {
+	setTimeout(function() {
+		$("#player-type-container").show("blind", {
+			"direction": "down"
+		}, 250);
+	}, 250);
+	$("#login-container").hide("blind", 500);
+	$("#register-container").hide("blind", 500);
 });
--- Server/main.js
+++ Server/main.js
@@ -2,4 +2,10 @@ var io = require('socket.io')(8081);
 
 io.on('connection', function(socket) {
 	console.log("A user has connected!");
+	
+	socket.emit('initial-connection');
+	
+	socket.on('disconnect', function() {
+		console.log("A user has disconnected!");
+	});
 });
